e3f41bd34986dbbd61094de2d60c658751e49a7d
Daniel Kats <d.kats@fkf.mpg.de>
2025-04-08 15:48:43 +0200

    Add pre-generated icons

--- build/generate-icons.sh
+++ build/generate-icons.sh
@@ -3,13 +3,35 @@
 # Create icons directory if it doesn't exist
 mkdir -p build/icons
 
-# Generate PNG icons in different sizes
-for size in 16 32 48 64 128 256 512; do
-  inkscape -w $size -h $size build/icon_benzene_simple.svg -o "build/icons/${size}x${size}.png"
+# Generate PNG icons in different sizes from SVG with transparency
+for size in 16 32 48 64 128 256 512 1024; do
+  magick "build/icon_benzene.svg" -background none -resize ${size}x${size} "build/icons/${size}x${size}.png"
 done
 
-# For Linux .ico file
-convert build/icons/256x256.png build/icon.ico
+# Generate Windows .ico file (using only standard Windows icon sizes)
+magick "build/icons/16x16.png" "build/icons/32x32.png" "build/icons/48x48.png" "build/icons/256x256.png" -background none "build/icon.ico"
 
-# For macOS .icns file (if needed)
-png2icns build/icon.icns build/icons/16x16.png build/icons/32x32.png build/icons/128x128.png build/icons/256x256.png build/icons/512x512.png
+# Generate macOS .icns file
+# Create temporary iconset directory
+mkdir -p build/icon.iconset
+cp build/icons/16x16.png build/icon.iconset/icon_16x16.png
+cp build/icons/32x32.png build/icon.iconset/icon_16x16@2x.png
+cp build/icons/32x32.png build/icon.iconset/icon_32x32.png
+cp build/icons/64x64.png build/icon.iconset/icon_32x32@2x.png
+cp build/icons/128x128.png build/icon.iconset/icon_128x128.png
+cp build/icons/256x256.png build/icon.iconset/icon_128x128@2x.png
+cp build/icons/256x256.png build/icon.iconset/icon_256x256.png
+cp build/icons/512x512.png build/icon.iconset/icon_256x256@2x.png
+cp build/icons/512x512.png build/icon.iconset/icon_512x512.png
+cp build/icons/1024x1024.png build/icon.iconset/icon_512x512@2x.png
+
+# If on macOS, use iconutil to create .icns
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  iconutil -c icns build/icon.iconset -o build/icon.icns
+else
+  # On Linux/Windows, use ImageMagick to create .icns
+  magick "build/icons/16x16.png" "build/icons/32x32.png" "build/icons/48x48.png" "build/icons/128x128.png" "build/icons/256x256.png" "build/icons/512x512.png" -background none "build/icon.icns"
+fi
+
+# Clean up temporary iconset directory
+rm -rf build/icon.iconset
